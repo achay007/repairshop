d6d2530b2069af0619fa682fd23cd7e89624afa5
achay007 <akshayachz09@gmail.com>
2025-06-26 13:51:37 +0530

    not found page, sentry error page

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,3 +1,6 @@
+export const metadata = {
+    title: 'Customers',
+}
 export default function Customers() {
     return (
        <h1>Customers page</h1>
--- src/app/(rs)/home/page.tsx
+++ src/app/(rs)/home/page.tsx
@@ -1,3 +1,6 @@
+export const metadata = {
+    title: 'Home',
+}
 export default function Home() {
     return (
        <h1>Home page</h1>
